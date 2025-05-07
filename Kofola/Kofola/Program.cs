26d3b9f103b0a0f5b31718b7de342bc10ef72847
dlabca <dlabca.dlabca@gmail.com>
2025-05-07 16:37:56 +0200

    Initial commit

---
+++ Kofola/Kofola/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Configuration.Assemblies;
+
+namespace Kofola
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        public static string VyresProblem(int cenaVelkeho, int objemVelkeho, int cenaMaleho, int objemMaleho)
+        {
+
+            string vysledek;
+            float pomerCenVelkeho = cenaVelkeho / (float)objemVelkeho;
+            float pomerCenMaleho = cenaMaleho / (float)objemMaleho;
+            if (pomerCenVelkeho <= pomerCenMaleho)
+                vysledek = "VETSI";
+            else
+                vysledek = "MENSI";
+            return vysledek;
+        }
+    }
+}
---
+++ kaiopea/kasiopea_dk_2024_d/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace MyApp
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            int cislo1 = 0;
+            int cislo2 = 0;
+            string vstup = Console.ReadLine();
+            int[] cisla = [.. from x in vstup.Split(' ') select int.Parse(x)];
+            foreach (int item in cisla)
+            {
+                
+            }
+        }
+    }
+}
