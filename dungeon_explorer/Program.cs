b0c2feb0ad5f1ef53471feb940a29c33a5dd1ecd
dlabca <dlabca.dlabca@gmail.com>
2025-05-18 19:49:41 +0200

    edited dungeon explorer

--- dungeon_explorer/Program.cs
+++ dungeon_explorer/Program.cs
@@ -14,25 +14,36 @@ namespace dungeon_explorer
     }
     class Game
     {
-        
+        Player player;
+        Room[,] map;
         public int mapWidth = 5;
         public int mapHeight = 5;
+        enum ItemType
+        {
+            Weapon,
+            Armor,
+            Consumable
+        }
 
         public void Start()
         {
             DisplayIntro();
             SetupRooms();
+            Console.Clear();
             Console.WriteLine("jak pojmenuješ svoji postavu?");
             string playerName = Console.ReadLine();
-            Player player = new Player(playerName, 100, 10, 0.1f);
+            player = new Player(playerName, 100, 10, 0.1f, new Position(0, 0));
+            Console.WriteLine($"Vítejte, {player.Name}!");
+            player.CurrentRoom = map[player.CurrentPosition.X, player.CurrentPosition.Y];
             Console.Clear();
             while (true)
             {
                 Console.WriteLine("> ");
+
                 string input = Console.ReadLine();
-                if (input.ToLower() == "konec")
+
+                if (input.ToLower() == "konec" || input.ToLower() == "exit")
                 {
-                    Console.WriteLine("Děkujeme za hraní!");
                     break;
                 }
                 else
@@ -41,27 +52,145 @@ namespace dungeon_explorer
                 }
             }
         }
-        void HandleCommand(string command)
+        void MovePlayer(string direction)
         {
-            switch (command.ToLower())
+            Position pos = player.CurrentPosition;
+
+            switch (direction.ToLower())
             {
                 case "nahoru":
-                    Console.WriteLine("Pohyb nahoru");
+                    if (pos.Y > 0) pos.Y--;
                     break;
                 case "dolu":
-                    Console.WriteLine("Pohyb dolů");
+                    if (pos.Y < mapHeight - 1) pos.Y++;
                     break;
                 case "doleva":
-                    Console.WriteLine("Pohyb doleva");
+                    if (pos.X > 0) pos.X--;
                     break;
                 case "doprava":
-                    Console.WriteLine("Pohyb doprava");
+                    if (pos.X < mapWidth - 1) pos.X++;
+                    break;
+                default:
+                    Console.WriteLine("Neplatný směr. Zadejte nahoru, dolu, doleva nebo doprava.");
+                    return;
+            }
+
+            player.CurrentPosition = pos;
+            player.CurrentRoom = map[pos.X, pos.Y];
+        }
+        void UseItem(String itemName)
+        {
+            Item item = player.Inventory.Find(i => i.Name.ToLower() == itemName.ToLower());
+            Console.WriteLine($"Používáte {item.Name}: {item.Description}");
+            if (item.Type == ItemType.Consumable)
+            {
+                Console.WriteLine($"Obnovujete {item.HealthRestore} HP.");
+                player.Health += item.HealthRestore;
+                if (item.Durability <= 0)
+                {
+                    Console.WriteLine($"Předmět {item.Name} se rozbil.");
+                    player.Inventory.Remove(item);
+                }
+            }
+            else if (item.Type == ItemType.Armor)
+            {
+                Console.WriteLine($"Získali jste {item.DamageReduction} poškození.");
+                player.EquippedArmor = item;
+                if (item.Durability <= 0)
+                {
+                    Console.WriteLine($"Předmět {item.Name} se rozbil.");
+                    player.Inventory.Remove(item);
+                    player.EquippedArmor = null;
+                }
+            }
+            else if (item.Type == ItemType.Weapon)
+            {
+                Console.WriteLine($"Získali jste {item.Damage} poškození.");
+                player.EquippedWeapon = item;
+                if (item.Durability <= 0)
+                {
+                    Console.WriteLine($"Předmět {item.Name} se rozbil.");
+                    player.Inventory.Remove(item);
+                    player.EquippedWeapon = null;
+                }
+            }
+            item.Durability--;
+        }
+        void HandleCommand(string command)
+        {
+            string[] commandParts = command.ToLower().Split(' ');
+            string action = commandParts[0];
+            string target = commandParts.Length > 1 ? string.Join(" ", commandParts, 1, commandParts.Length - 1) : null;
+
+            switch (action)
+            {
+                case "jdi":
+                    MovePlayer(target);
                     break;
                 case "inventar":
                     Console.WriteLine("Zobrazit inventář");
+                    foreach (Item item in player.Inventory)
+                    {
+                        Console.WriteLine($"-{item.Name}: {item.Description}");
+                    }
                     break;
                 case "pouzij":
                     Console.WriteLine("Použít předmět");
+                    UseItem(target);
+                    break;
+                case "vezmi":
+                    Item itemToTake = player.CurrentRoom.Items.Find(i => i.Name.ToLower() == target.ToLower());
+                    if (itemToTake != null)
+                    {
+                        player.Inventory.Add(itemToTake);
+                        player.CurrentRoom.Items.Remove(itemToTake);
+                        Console.WriteLine($"Vzal jsi {itemToTake.Name}.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Předmět {target} nebyl nalezen.");
+                    }
+                    break;
+                case "prozkomat":
+                    Console.WriteLine($"Prozkoumáváte {player.CurrentRoom.Name}: {player.CurrentRoom.Description}");
+                    if (player.CurrentRoom.Enemies.Count > 0)
+                    {
+                        Console.WriteLine("V místnosti jsou následující nepřátelé:");
+                        foreach (Enemy enemy in player.CurrentRoom.Enemies)
+                        {
+                            Console.WriteLine($"-{enemy.Name}");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("V místnosti nejsou žádní nepřátelé.");
+                    }
+                    if (player.CurrentRoom.Items.Count > 0)
+                    {
+                        Console.WriteLine("V místnosti jsou následující předměty:");
+                        foreach (Item item in player.CurrentRoom.Items)
+                        {
+                            Console.WriteLine($"-{item.Name}: {item.Description}");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("V místnosti nejsou žádné předměty.");
+                    }
+                    break;
+                case "statystiky":
+                    Console.WriteLine($"Jméno: {player.Name}");
+                    Console.WriteLine($"Zdraví: {player.Health}");
+                    Console.WriteLine($"Útočná síla: {player.BaseAttackPower}");
+                    Console.WriteLine($"Šance na kritický zásah: {player.ChanceToCriticalHit * 100}%");
+                    if (player.EquippedArmor != null)
+                    {
+                        Console.WriteLine($"Vybraná zbroj: {player.EquippedArmor.Name}");
+                    }
+                    if (player.EquippedWeapon != null)
+                    {
+                        Console.WriteLine($"Vybraný zbraň: {player.EquippedWeapon.Name}");
+                    }
                     break;
                 default:
                     Console.WriteLine("Neznámý příkaz.");
@@ -98,11 +227,8 @@ namespace dungeon_explorer
         void PrintControls()
         {
             Console.WriteLine("Ovládání:");
-            Console.WriteLine("Nahoru - pohyb nahoru");
-            Console.WriteLine("Dolu - pohyb dolů");
-            Console.WriteLine("Doleva - pohyb doleva");
-            Console.WriteLine("Doprava - pohyb doprava");
-            Console.WriteLine("Konec - ukončit hru");
+            Console.WriteLine("Jdi [nahoru/dolu/doleva/doprava] - pohyb");
+            Console.WriteLine("Konec(nebo exit) - ukončit hru");
             Console.WriteLine("Inventar - inventář");
             Console.WriteLine("Pouzij - použít předmět");
             Console.WriteLine("Stiskněte libovolnou klávesu pro pokračování...");
@@ -110,10 +236,9 @@ namespace dungeon_explorer
         }
         void SetupRooms()
         {
-            Room[,] map = new Room[mapWidth, mapHeight];
+            map = new Room[mapWidth, mapHeight];
             map[0, 0] = new Room("Hlavní místnost", "Toto je hlavní místnost dungeonu.");
-            map[0, 0].IsTherePlayer = true;
-            map[0, 0].Items.Add(new Item("Léčivý lektvar", "Obnovuje 20 HP", 20, 0, 1, 0));
+            map[0, 0].Items.Add(new Item("Léčivý lektvar", "Obnovuje 20 HP", 20, 0, 1, 0, ItemType.Consumable));
             for (int i = 0; i < mapWidth; i++)
             {
                 for (int j = 0; j < mapHeight; j++)
@@ -131,7 +256,6 @@ namespace dungeon_explorer
             public string Description { get; set; }
             public List<Enemy> Enemies { get; set; }
             public List<Item> Items { get; set; }
-            public bool IsTherePlayer { get; set; }
             public Room(string name, string description)
             {
                 Name = name;
@@ -149,13 +273,18 @@ namespace dungeon_explorer
             public List<Item> Inventory { get; set; }
             public Item EquippedArmor { get; set; }
             public Item EquippedWeapon { get; set; }
-            public Player(string name, int health, int attackPower, float chanceToCriticalHit)
+            public Room CurrentRoom { get; set; }
+            public Position CurrentPosition { get; set; }
+
+
+            public Player(string name, int health, int attackPower, float chanceToCriticalHit, Position currentPosition)
             {
                 Name = name;
                 Health = health;
                 BaseAttackPower = attackPower;
                 ChanceToCriticalHit = chanceToCriticalHit;
                 Inventory = new List<Item>();
+                CurrentPosition = currentPosition;
             }
         };
         class Enemy
@@ -180,17 +309,31 @@ namespace dungeon_explorer
             public int Damage { get; set; }
             public int Durability { get; set; }
             public int DamageReduction { get; set; }
+            public ItemType Type { get; set; }
 
 
-            public Item(string name, string description, int healthRestore, int damage, int durability, int damageReduction)
+            public Item(string name, string description, int healthRestore, int damage, int durability, int damageReduction, ItemType itemType)
             {
                 Name = name;
                 Description = description;
                 HealthRestore = healthRestore;
                 Damage = damage;
                 Durability = durability;
-
+                DamageReduction = damageReduction;
+                Type = itemType;
             }
         }
     }
+    public struct Position
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Position(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+
 }
